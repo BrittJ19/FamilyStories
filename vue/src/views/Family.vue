eac2ff210a29f28d52289e337951a137b1976c41
Ayesha Stewart <e_shaneef@yahoo.com>
2022-04-11 14:46:23 -0400

    capstone

---
+++ vue/src/views/Family.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class ="family">
+      <p>
+          <router-link v-bind:to="{ name: 'parentAccount' }">Add a Parent Account</router-link>&nbsp;|&nbsp;
+          <router-link v-bind:to="{name: 'childAccount' }"> Add a Child Account</router-link>
+      </p>
+      </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
