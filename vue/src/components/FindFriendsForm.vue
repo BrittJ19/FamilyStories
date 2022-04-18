979c2f3318bd131e065b10803869552f58eaab0f
Zachary Ford <zacharyford13@gmail.com>
2022-04-18 10:27:35 -0400

    almost there for friends

--- vue/src/components/FindFriendsForm.vue
+++ vue/src/components/FindFriendsForm.vue
@@ -2,28 +2,28 @@
   
 <div class= "find_friends">
     <head>
-    <h2>Find Friends</h2>
+    <h1>Find Friends</h1>
     </head>
-      <form v-on:submit.prevent="addToFamily">
+      <form v-on:submit.prevent="addToFriends">
           <div id="searchBox" class="form-element">
               <label for="account-name"> Search user by username : </label>
               <input id="name-text" type="text" v-model="text"/>
               <p>Type all or part of username.</p>
           </div>    
 
-          <!-- <div class="form-element">
-              <label for="account-type"> Account Type:</label>
-                  <select id="selections">
-                      <option value="parent">Parent</option>
-                      <option value="child">Child</option>
-                  </select>
-          </div>  -->
           <div id="search" class="form-element">
-              <button id="searchButton" v-on:click="searchForUsers">Search Users to Add</button>
+              <button id="searchButton" v-on:click="searchForUsers">Search for Friends to Add</button>
               <select name="results" id="results" v-if="isSearching">
                 <option value="username" v-for="user in searchResults" v-bind:key="user.username">{{user}}</option>
               </select>
           </div> 
+          <select name="results" id="accountNames" v-on:change="saveAccountSelection()">
+              <option disabled selected value>-- select an option --</option>
+                <option value="username" v-for="account in userAccounts" v-bind:key="account.user_id">{{account.username}}</option>
+              </select>
+          <div class="actions">
+              <button id="submit" type="submit">Add Friend</button>
+          </div>
     </form>
 </div>
   
@@ -34,6 +34,20 @@
 import databaseService from '../services/DatabaseService'
 export default {
     name:"find_friends",
+    data(){
+        return{ message: "",
+            isLoading: '',
+            text1: '',
+            search: '',
+            users: [],
+            text: '',
+            searchResults: [],
+            searchMessage:"",
+            isSearching: false,
+            myFriends: [],
+            memberAdded: ''
+        }
+    },
 methods: {
             updateSearchFilter() {
                 this.$store.commit('UPDATE_SEARCH', this.searchTerm);
@@ -55,21 +69,21 @@ methods: {
             },
             selectAccount() {
                 var account = document.getElementById("accountNames").value;
-                this.familyAccount.familyName = account;
+                this.userAccount.userame = account;
             },
 
             selectUser() {
                 var user = document.getElementById("results").value;
-                this.familyAccount.username = user;
+                this.userAccount.username = user;
             },
             addToFriends() {
                 var selectedUser = document.getElementById('results');
                 var userValue = selectedUser.options[selectedUser.selectedIndex].text;
-                this.familyAccount.username = userValue;
+                this.username = userValue;
                 var selectAccount = document.getElementById('accountNames');
                 var accountValue = selectAccount.options[selectAccount.selectedIndex].text;
-                this.familyAccount.familyName = accountValue;
-                databaseService.addMemberToFamily(this.familyAccount).then( resp => {
+                this.userAccount.userName = accountValue;
+                databaseService.addToFriends(this.username).then( resp => {
                     if(resp.status == 200){
                         this.memberAdded = true;
                     }else {
@@ -98,245 +112,107 @@ methods: {
 @import url('http://fonts.cdnfonts.com/css/abeatbykai');
 @import url('https://fonts.googleapis.com/css2?family=Amaranth:wght@700&family=Montserrat+Alternates:wght@100&display=swap');
 
-#options{
-  display: flex;
-  height: 10px;
-  align-self: flex-end;
-  margin-bottom: 10px
+#name-text{
+    height: 30px
 }
 
-#search-button{
-  width: 100%;
-  margin-left: 5px;
+#searchButton{
+    margin-bottom: 80px;
+    width: 200px
 }
 
-#promo{
-   display: flex;
-   flex-grow: 1;
-   background-color: rgb(150,165,60);
-   justify-self: center;
-   justify-content: center;
-   color: rgb(245,245,220);
-   font-family: 'abeatbyKai', sans-serif;
-   margin-top: 10px;
- }
+#searchBox{
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    align-self: center;
+    font-family: 'abeatbyKai', sans-serif;
+    color: rgb(245,245,220);
+}
+p{
+    font-family: 'abeatbyKai', sans-serif;
+    color: rgb(245,245,220);
+    align-self: center;
+}
+
+.form-element{
+    padding: 10px;
+}
 
 #submit{
-  /* display: flex;
-  border-radius: 10%;
-  width: 100%;
-  height: 0;
-  justify-content: center;
-  justify-self: center;
-  align-self: center; */
-  background-color: rgb(150,165,60);
-  font-size: 15px;
-  font-family: 'abeatbyKai', sans-serif;
-  color: rgb(245,245,220);
-  padding: 10px;
-  border-radius: 10%;
-  /* padding: 20px;
-  color: rgb(245,245,220);
-  margin-right: 5px; */
-  padding-right: 18px;
-  padding-left: 18px;
+    margin-top: 50px;
+    width: 200px;
+    margin-top: 200px
 }
 
-#troubleshoot{
-  display: flex;
-  flex-grow: 1;
-  text-decoration: none;
-  color: rgb(255,196,12);
-  height: 10px;
+#newUser{
+    margin-top: 0px;
+    width: 200px;
+    margin: 40px
 }
 
-/* #books{
-  height: 400px;
-  width: 180px;
-} */
-
-/* #buttons{
-  display: flex;
-  flex-direction: column;
-  padding: 10px;
-  font-family: 'abeatbyKai', sans-serif;
-} */
-
-#needAccount{
-  display: flex;
-  flex-grow: 1;
-  height: 5px;
-  padding-bottom: 10px;
-  color: rgb(255,196,12);
-  text-decoration: none;
-  justify-content: flex-start;
-  align-content: flex-start;
-  align-self: flex-start;
-  font-family: 'abeatbyKai', sans-serif;
-  margin-bottom: 100px;
-  margin-left: 7px;
-  margin-right: 50px
+.actions{
+    display: flex;
+    flex-direction: column;
+    flex-direction: column-reverse;
+    padding: 20px
 }
 
-#pic{
-  display: flex;
+#results{
+    width: 25%;
+    align-self: center;
+    height: 40px
+
+}
+#accountNames{
+    width: 25%;
+    align-self: center;
+    height: 40px
+
 }
 
-#left{
-  display: flex;
-  flex-grow: 1;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-content: flex-end;
-  margin-right: 20px
+#search{
+    display: flex;
+    flex-direction: column;
 }
 
-#innerNav{
-  display: flex;
-  align-content: center;
-  justify-content: center;
-  margin-bottom: 30px;
+form{
+    display: flex;
+    flex-direction: column;
+    padding: 50px
+}
+body{
+    background-color: rgb(245,245,220);
+}
+#all{
+    background-color:rgb(245,245,220);  
 }
 
-#site-links{
-  display: flex;
-  flex-grow: 1;
-  padding-top: 10px;
-  text-decoration: none;
-  justify-content: flex-end;
-  align-content: flex-end;
-  /* justify-content: space-between; */
-  font-family: 'abeatbyKai', sans-serif;
-  
+label{
+    font-family: 'abeatbyKai', sans-serif;
+    padding: 5px;
+    color:rgb(245,245,220);
+}
+h1{
+    display: flex;
+    justify-content: center;
+    color:rgb(245,245,220);
+    font-family: 'abeatbyKai', sans-serif;
 }
 
-.form-signin{
-  display: flex;
-  align-items: stretch;
-  justify-content: center;
-  align-content: center;
-  align-self: center;
-  height: 40px;
-  margin-bottom: 30px;
-}
-
-.sr-only{
-  display: flex;
-}
-
-#login{
-  display: flex;
-  flex-direction: column;
-  /* flex-grow: 1; */
-  justify-content: flex-end;
-  align-content: flex-end;
-  align-self: flex-end;
-  background-color: rgb(255,117,24);
-  font-family: 'abeatbyKai', sans-serif;
-  /* font-family: 'Montserrat Alternates', sans-serif;
-  color: black; */
-  
-}
-
-.sr-only{
-display: flex;  
-background-color: rgb(255,196,12);
-border: 4px solid rgb(150,165,60);
-padding: 10px;
-margin: 5px;
-justify-content: center;
-align-content: center;
-align-self: center;
-border-radius: 10px;
-/* color: rgb(245,245,220); */
-}
-
-input{
-  border: 4px solid rgb(150,165,60);
-  border-radius: 5%
-}
-
-#catchphrase{
-   direction: flex;
-   height: 160px;
-   width: 560px;
-   justify-content: flex-start;
-   align-content: flex-start;
- }
-
-#logo{
-   width: 180px;
-   height: 120px;
-   display: flex;
-   align-self: center;
-  color: rgb(245,245,220);
-  filter: opacity(0.5) drop-shadow(0,0,0 rgb(150,165,60));
- }
-
-#familyReading{
-  /* display: flex; */
-   width: 100%;
- }
-
-#banner{
-  display: flex;
-   background-color: rgb(255,117,24);
-   flex-direction: column;
+#page{
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    background-color:rgb(245,245,220);
 }
 
 #main{
-  display: flex;
-  flex-direction: column;
-}
-
-#nav{
-  display: flex;
-  background-color: rgb(255,117,24);
-  /* flex-grow: 1; */
-}
-
-#familyReading{
-  widows: 100px;
-}
-
-#home{
-  text-decoration: none;
-  color: rgb(245,245,220);
-  font-size: 20px;
-  padding: 20px;
-  padding-right: 50px;
-  border-right: 4px solid rgb(150,165,60);
-}
-
-#home:hover{
-  font-size: 25px;
-}
-
-#about{
-  text-decoration: none;
-  color: rgb(245,245,220);
-  font-size: 20px;
-  padding: 20px;
-  padding-right: 50px;
-  margin-left: 30px;
-  border-right: 4px solid rgb(150,165,60);
-}
-
-#about:hover{
-  font-size: 25px;
-}
-
-#charities{
-  text-decoration: none;
-  color: rgb(245,245,220);
-  font-size: 20px;
-  padding-top: 20px;
-  margin-right: 10px;
-  margin-left: 50px;
-}
-
-#charities:hover{
-  font-size: 25px;
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    flex: 1 0 auto;
+  
 }
 
 </style>
