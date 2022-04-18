2bca2500729c0e0cf6c25d91beb5620ccbb1097d
Andre <avalines22@gmail.com>
2022-04-18 12:04:03 -0400

    updates check activity

--- vue/src/components/CheckReadingActivity.vue
+++ vue/src/components/CheckReadingActivity.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Amaranth:wght@700&family=Montserrat+Alternates:wght@100&display=swap" rel="stylesheet">
-        <link href="http://fonts.cdnfonts.com/css/abeatbykai" rel="stylesheet">             
+        <link href="https://fonts.googleapis.com/css2?family=Amaranth:wght@700&family=Montserrat+Alternates:wght@100&display=swap" rel="stylesheet"> -->
+        <!-- <link href="http://fonts.cdnfonts.com/css/abeatbykai" rel="stylesheet">              -->
+    <div id="nav">
+      <img id="logo" src="/offWhiteLogo.png">
+      <img id="catchphrase" src="/logoText.png"> 
+      <!-- <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp; -->
+      <!-- <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link> -->
+      </div>
+      <div id="left">
+        <!-- <img id="books" src="/books.png"> -->
+      </div>  
+      <div id="innerNav">
+        <div id="site-links">
+        <router-link id="home" v-bind:to="{ name: 'home' }">Home </router-link>
+        <router-link id="about" v-bind:to="{name: 'aboutUs'}"> About Us </router-link>
+        <router-link id="charities" v-bind:to="{name: 'charities'}"> Charities We Work With </router-link>
+        </div> 
+  </div>
+    
     </head>
+  
     <table id="CheckActivity">
       <thead>
         <tr>
@@ -17,7 +35,20 @@
         </tr>
       </thead>
       <tbody>
+        <tr>
+          <td>Andre</td>
+          <td>"Outwitting the Devil"</td>
+          <td>44</td>
+          <td>"The color purple"</td>
+        </tr>
+        <tr>
+          <td>Ayesha</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          
 
+        </tr>
       </tbody>
 
     </table>
@@ -35,16 +66,32 @@ export default {
 
 <style scoped>
 
-table {
-  font-family: Arial, Helvetica, sans-serif;
-  border-color: 10px, solid, rgb(150,165,60);
+#CheckActivity {
+  
+  font-family: Arial;
+  border: 5px solid black;
   margin: 10px;
 
 }
 th {
-  text-align: center;
+  text-align: left;
   text-decoration: underline;
+  border-bottom: 5px solid black;
   border: 10px;
   margin: 10px;
 }
+
+tbody {
+  text-align: left;
+  border-bottom: 5px solid black;
+  margin: 10px;
+}
+#logo{
+   width: 180px;
+   height: 120px;
+   display: flex;
+   align-self: center;
+  color: rgb(245,245,220);
+  filter: opacity(0.5) drop-shadow(0,0,0 rgb(150,165,60));
+ }
 </style>
